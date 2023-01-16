33df1f4fe0ad8fe1861cd94885f586676058c6dd
hashirnp <96100096+hashirnp@users.noreply.github.com>
2023-01-16 07:57:44 +0530

    notesrouter

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 // app.use('/notes', notesRoutes)
-app.use(subdomain('/notes', apiRouter))
+app.use(subdomain('/notes', notesRoutes))
 
 
 app.use('/', (req, res) => {
